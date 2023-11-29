1f49a2e42188cefe3bec53ab87cce15493400b6c
Piotr Kubon <pkubon@interia.eu>
2023-11-29 21:01:37 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        wypisz(5);
+    }
+    public static void wypisz(int n){
+        for(int i = 1;i<=n;i++) {
+            System.out.println(i);
+
+        }
+    }
+}
